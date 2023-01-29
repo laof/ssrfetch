5bb1473de9034816ba32c0478bbb29ecf3281604
laof <345603828@qq.com>
2023-01-29 12:23:57 +0800

    54

--- test/test.js
+++ test/test.js
@@ -3,9 +3,7 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.baidu.com", {
-    waitUntil: "networkidle2",
-  });
+  await page.goto("https://www.baidu.com");
   await page.pdf({ path: "test.pdf", format: "a4" });
 
   await browser.close();
