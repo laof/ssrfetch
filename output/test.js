840691ca4d72d434620918d9e931b552c184df7f
laof <345603828@qq.com>
2023-01-29 05:13:26 +0000

    ok

--- output/test.js
+++ output/test.js
@@ -1,10 +1,15 @@
 const puppeteer = require("puppeteer");
 
+
+console.log(__dirname);
+
+console.log(__filename);
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://www.baidu.com");
   await page.pdf({ path: "test/test.pdf", format: "a4" });
-//   await page.pdf({ path: "/test/test3.pdf", format: "a4" });
+  //   await page.pdf({ path: "/test/test3.pdf", format: "a4" });
   await browser.close();
 })();
