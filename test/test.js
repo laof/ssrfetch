b3f11c75c11ca5ea1d4cc6f73f6615eb1c70acc6
laof <345603828@qq.com>
2023-01-29 14:32:30 +0800

    cicd:ok

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ const fs = require("fs");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://lncn.org");
+  await page.waitForSelector(".ssr-btn-bar button");
   const html = await page.content();
   fs.writeFileSync("test.txt", html);
   await browser.close();
