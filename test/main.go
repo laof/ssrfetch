d8bed7ce598bab9410f1759c3709b63c8b5aae65
laof <345603828@qq.com>
2023-01-29 11:31:15 +0800

    r6

--- test/main.go
+++ test/main.go
@@ -9,12 +9,11 @@ import (
 )
 
 func main() {
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", true),
-		chromedp.Flag("disable-gpu", false),
-		chromedp.Flag("enable-automation", false),
-		chromedp.Flag("disable-extensions", false),
-	)
+	opts := append(chromedp.DefaultExecAllocatorOptions[:])
+	// chromedp.Flag("headless", true),
+	// chromedp.Flag("disable-gpu", false),
+	// chromedp.Flag("enable-automation", false),
+	// chromedp.Flag("disable-extensions", false),
 
 	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
@@ -22,13 +21,14 @@ func main() {
 	// create context
 	ctx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
 	defer cancel()
-	var res string
+	var res string = "="
 
-	if err := chromedp.Run(ctx,
+	err := chromedp.Run(ctx,
 		chromedp.Navigate(`https://lncn.org/`),
 		chromedp.OuterHTML(`body`, &res, chromedp.NodeVisible, chromedp.ByQuery),
-	); err != nil {
-		log.Fatal(err)
+	)
+	if err != nil {
+		return
 	}
 
 	fmt.Println(res)
