244ea697b13becb6bdadcf9a2c75b5b711c569b6
laof <345603828@qq.com>
2023-01-29 05:20:10 +0000

    ok

--- output/test.js
+++ output/test.js
@@ -20,7 +20,7 @@ console.log("cwd       : " + process.cwd());
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://www.baidu.com");
-  await page.pdf({ path: "test/test.pdf", format: "a4" });
+  await page.pdf({ path: __dirname + "test.pdf", format: "a4" });
   //   await page.pdf({ path: "/test/test3.pdf", format: "a4" });
   await browser.close();
 })();
