3f11d3613ffc5c626d3ae17c866f06b97b0bad6a
laof <345603828@qq.com>
2023-01-29 14:09:33 +0800

    cicd:ok

--- test/test.js
+++ test/test.js
@@ -2,11 +2,7 @@ const puppeteer = require("puppeteer");
 const { resolve, join } = require("path");
 
 (async () => {
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox"],
-    executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
-    headless: false,
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://www.baidu.com");
   await page.pdf({ path: "test.pdf", format: "a4" });
