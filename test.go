4e4e98640f47193ceba507db964ac55f6d824ae8
laof <345603828@qq.com>
2023-02-21 08:44:27 +0800

    Create test.go

---
+++ test.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/bitly/go-simplejson"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+const key = `
+test
+`
+
+type yamlData struct {
+	Key   string
+	Value string
+}
+
+/*
+CreateSigned test
+*/
+func CreateSigned(jsonstr string) (tokenStr string, err error) {
+
+	js, err := simplejson.NewJson([]byte(jsonstr))
+
+	if err != nil {
+		return "", err
+	}
+
+	maping, err := js.Map()
+
+	if err != nil {
+		return "", err
+	}
+
+	jsonmap := jwt.MapClaims{
+		"timestamp": time.Now().Add(time.Hour*24).UnixNano() / 1e6,
+	}
+
+	for k := range maping {
+		jsonmap[k] = maping[k]
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jsonmap)
+
+	token.Header = map[string]interface{}{
+		"kid": "AWS",
+		"alg": jwt.SigningMethodRS256.Alg(),
+	}
+	privateKey, _ := jwt.ParseRSAPrivateKeyFromPEM([]byte(key))
+
+	return token.SignedString(privateKey)
+}
